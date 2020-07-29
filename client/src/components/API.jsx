348799cb7fdf816b2460f7495beec7f2e3fa35dc
mhlgoossens <mhlgoossens@gmail.com>
2020-07-30 00:13:47 +0200

    succeed with 'hello world' api call

--- client/src/components/API.jsx
+++ client/src/components/API.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 
 class API extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            apiResponse: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiResponse: '',
+    };
+  }
 
-    callAPI() {
-        fetch('http://localhost:9000/test')
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-    }
+  componentDidMount() {
+    this.callAPI();
+  }
 
-    componentDidMount() { 
-        this.callAPI();
-    }
+  callAPI() {
+    fetch('http://localhost:9000/test')
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
+  }
 
-    render() {
-        return (
-            <div className="APItest">
-                Hello from API test jsx
-                <p className="backend">{this.state.apiResponse}</p>
-            </div>
-        )
-    }
+  render() {
+    const { apiResponse } = this.state;
+    return (
+      <div className="APItest">
+        Hello from API test jsx
+        <p className="backend">{apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default API;
