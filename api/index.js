e3ef44a0d0ffcfa45c78a8af4ccc35b9443fa1a7
mhlgoossens <mhlgoossens@gmail.com>
2020-07-29 23:11:29 +0200

    add middleware to express boilerplate

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const testRouter = require('./routes/testAPI');
 
 const app = express();
 const port = 9000;
 
+// middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+// routes
+app.use('/test', testRouter);
 app.get('/', (req, res) => res.send('Hello World!'));
 
 // eslint-disable-next-line no-console
