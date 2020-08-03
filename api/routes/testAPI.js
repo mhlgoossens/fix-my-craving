f4704c975ca2f9d43f9c53a8c2498aee23977728
Marc <mhlgoossens@gmail.com>
2020-08-03 10:10:24 +0200

    Testing ubuntu setup

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+// testing ubuntu and git
 router.get('/', (req, res) => {
   res.send('Testing first API route, backend talking to you!');
 });
